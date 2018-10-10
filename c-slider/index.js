3de3a9169a365f6e80dc2d78c2f587c33b80e985
chenyong <1015561170@qq.com>
2018-10-10 22:30:54 +0800

    add: c-slider组件

--- c-slider/index.js
+++ c-slider/index.js
@@ -1,23 +1,71 @@
 // mini-program-components/c-slider/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
   properties: {
-    
+    //要渲染的数据
+    lists: {
+      type: Array,
+      value: []
+    },
+    //单个按钮的宽度
+    singleBtnW: { 
+      type: Number,
+      value: 0
+    },
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    startX: 0,
+    sliderStatus: false,
+    sliderClickNum: 0,
   },
-
-  /**
-   * 组件的方法列表
-   */
+  lifetimes: {
+  },
   methods: {
+    touchS: function (e) {
+      console.log('attached: ', this.properties.lists);
+      if (e.touches.length === 1) {
+        this.data.startX = e.touches[0].clientX;
+        this.properties.lists.forEach(item => {
+          item.style = 'left: 0';
+        });
+        this.setData({
+          lists: this.properties.lists
+        });
+      }
+    },
+    touchM: function (e) {
+      if (e.touches.length === 1) {
+        this.data.sliderStatus = true;
 
+        let disX = this.data.startX - e.touches[0].clientX,
+          index = e.currentTarget.dataset.index,
+          style = '';
+        if (disX <= 0) {
+          style = 'left: 0';
+        } else if (disX > 0) {
+          style = `left: -${disX}rpx`;
+          if (disX >= this.properties.singleBtnW) {
+            style = `left: -${this.properties.singleBtnW}rpx`;
+          }
+        }
+        this.properties.lists[index].style = style;
+        this.setData({
+          lists: this.properties.lists
+        });
+      }
+    },
+    touchE: function (e) {
+      if (e.changedTouches.length === 1) {
+        let disX = this.data.startX - e.changedTouches[0].clientX,
+          index = e.currentTarget.dataset.index,
+          style = (disX > this.properties.singleBtnW / 2) ? `left: -${this.properties.singleBtnW}rpx` : 'left: 0';
+        this.properties.lists[index].style = style;
+        this.setData({
+          lists: this.properties.lists
+        });
+      }
+    },
   }
 })
